ffe408597edda4f278d79ec981b6d98dfffe745c
ymmivstoml <talha.ortky@gmail.com>
2022-12-20 20:44:34 +0300

    FLAKE( ERORR AGAİN:((((

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,4 @@
 from django.test import SimpleTestCase
-
 from app import calc
 
 class CalcTests(SimpleTestCase):
@@ -9,7 +8,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
 
